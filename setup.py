2e41145871b340d3355f7c9c8532b2cc9a5bddb2
Daniel Sousa <sousa.dfs@gmail.com>
2017-03-01 11:49:16 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='opensubz',
-    version='0.1.2',
+    version='0.1.3',
     description='Easily search opensubtitles subtitles',
     long_description=long_description,
     url='https://github.com/danielfsousa/opensubz',
